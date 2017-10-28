7b05e62abbf4a473133b18fa65e214cae4dc06e5
Daniel <danielcarvajalpatino@gmail.com>
2017-10-28 09:50:38 -0500

    Index

---
+++ config.py
@@ -0,0 +1,22 @@
+#Librerias a usar
+import os
+
+#   Las configuraciones se agrupan por funcionamineto
+#   y se agrupan en clases.
+#   'Cada clase es un set de configuraciones'
+
+#Clase princial --Configuraciones en comun--
+class Config(object):
+
+    #Genera identificador para el formulario
+    SECRET_KEY = 'SECRET_KEY'
+
+#Clase Desarrollador --Configuraciones para el modo desarrollador--
+class DevelopmentConfig(Config):
+
+    #El servidor esta a la escucha de cambios
+    DEBUG = True
+
+    #Conexion con la base de datos
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/duntidb'
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False
--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+# Libraries
 from flask import Flask
+from flask import render_template
+from flask_wtf import CSRFProtect
+from config import DevelopmentConfig
 
+# Init Flask
 app = Flask(__name__)
 
-@app.route('/')
+# Implement config
+app.config.from_object(DevelopmentConfig)
+
+# Implement Security for forms
+csrf = CSRFProtect()
+
+# Rute /
+@app.route('/', methods=['GET'])
 def index():
-    return "Hellow word"
+    return render_template('index.html')
+
+@app.route('/stations' , methods=['POST'])
+def stations():
+    return "stations"
+
+@app.route('/report' , methods=['GET'])
+def report():
+    return "report"
+
+@app.route('/admin' , methods=['GET', 'POST'])
+def admin():
+    return "admin"
+
+@app.route('/reports' , methods=['GET'])
+def reports():
+    return "reports"
 
 if __name__ == '__main__':
-        app.run()
+    csrf.init_app(app)
+    app.run()
