eea0d061326315901be5bd8d4883c831f0963555
Daniel <danielcarvajalpatino@gmail.com>
2017-11-20 14:49:42 -0500

    Sugerencias por gustavo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Libraries
+import time
 from flask import Flask, redirect, url_for
 from flask import render_template
 from flask_wtf import CSRFProtect
@@ -70,20 +71,21 @@ def report(id_estacion = None):
 
         ## ID id_estacion exist in the db
 
-        return render_template('form.html', id_estacion=id_estacion)
+        return render_template('form.html', id_estacion=id_estacion, fecha=time.strftime("%d-%m-%Y") )
 
     if request.method == 'POST':
         img = getImageToB64(request.form['img'])
         mpimg.imsave("./static/img/rpt/"+"REPORT_"+request.form['id']+"_"+request.form['fecha_report']+".jpg",img, format="jpg", dpi=150)
-        informe = Reporte("REPORT"+request.form['id']+request.form['ubicacion'], request.form['id'], request.form['estado'], request.form['fecha_report'], request.form['nombre'], "./static/img/rpt/"+"REPORT_"+request.form['id']+"_"+request.form['fecha_report']+".jpg" ,request.form['ubicacion'])
+        informe = Reporte("REPORT"+request.form['id']+request.form['ubicacion']+time.strftime("%d/%m/%Y:%H:%M:%S"), request.form['id'], request.form['estado'], request.form['fecha_report'], request.form['nombre'],request.form['correo'], "./static/img/rpt/"+"REPORT_"+request.form['id']+"_"+request.form['fecha_report']+".jpg" ,request.form['ubicacion'])
         db.session.add(informe)
         db.session.commit()
         return json.dumps({'ok':200})
 
-@app.route('/admin' , methods=['GET', 'POST'])
+@app.route('/gestion' , methods=['GET', 'POST'])
 def admin():
     informes = Reporte.query.all() ##ARREGLAR QUERY
-    return render_template('admin.html', informes = informes)
+    estaciones = Estacion.query.all()
+    return render_template('admin.html', informes = informes, estaciones = estaciones)
 
 @app.route('/qrdecode', methods=['POST'])
 def qrdecode():
--- static/js/script.js
+++ static/js/script.js
@@ -132,51 +132,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 
-//    function agregaFormularioParaReporte(id_estacion){
-//      my_stream.stop();
-//      $("#qr").remove()
-//      $(".row").load('/static/html/form.html', ()=>{
-//
-//        if (navigator.geolocation) {
-//          navigator.geolocation.getCurrentPosition(success, error);
-//        }else {
-//          $("#ubicacion").val("Geolocation is not supported by this browser");
-//
-//        }
-//
-//        $("#id_form").val(id_estacion)
-//        $("#ubicacion").val()
-
-//        var hoy = new Date()
-//        $("#fecha_report").val(String(hoy.getDate())+"-"+String(hoy.getMonth()+1)+"-"+String(hoy.getFullYear()));
-
-//        $("#report").on('click', (ev)=>{
-//          ev.preventDefault()
-//          data = {}
-//          $('form').find(":input").each((i, val)=>{
-//            console.log("ID: "+val.id+" Value: "+val.value);
-//            console.log(val);
-//            if(val.id=="photo"){
-//              console.log(val);
-//            }
-//          });
-//
-//        })
-
-
-//          });
-      // ICI
-
-
-
-//    }
-
-
-
-
-
-
-
     function showVideo(){
         // Display the video stream and the controls.
 
